07f57473879110f98df088cdf472b12268b74e54
aqsamalic <aqsamalikurl@gmail.com>
2025-08-05 14:09:15 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-blue-600 text-3xl">
+      Tailwind is working!
+    </div>
+  );
+}
+
+export default App;
